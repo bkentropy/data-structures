95f2d85ef57133cd33bc5470e4ca4b09aae3ce97
Brian Kustra <bkustrakb@gmail.com>
2015-10-10 13:15:48 -0500

    Collision is working, but we want to review it.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,29 +1,28 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
-  this._duplicate = false;
+  this._tuple = {}; // Note: is this cool? okay? 
 };
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  if(this._storage[i]) {
-    i += 1;
-    this._duplicate = true;
+  if(i === 0) {
+     this._tuple[k] = v;
   }
-  this._storage[i] = v;
+  this._storage[i] = [k,v]; // <- this is the tuple/bucket
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  if(this._duplicate){
-    return this._storage[i+1];
+  if(this._tuple[k]){
+    return this._tuple[k];
   }
-  return this._storage[i];
+  return this._storage[i][1];
 };
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage[i] = null;
+  this._storage[i][1] = null;
 };
 
 /*
